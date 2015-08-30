34e355d57e9fb0d00d47090b54f58a45f897b7f8
chinuno-usami <luisaojiao@gmail.com>
2015-08-30 21:25:28 +0800

    ui update

--- biuflower.py
+++ biuflower.py
@@ -6,96 +6,126 @@ import urllib2
 import urllib
 import zlib
 import json
+import os
+
 def post(req,data):  
     '''Send post resquest
     '''
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())  
     response = opener.open(req, data) 
-    # print response.read().decode('utf-8') 
     global respInfo
     respInfo = response.info()
     return response.read()
 
+#setting
+class Setting_window:
+    def __init__(self, parent):
+        top = self.top = Toplevel(parent)
+        top.geometry('260x110')  
+        input_frm = Frame(top)
+        self.bkn_lab = Label(input_frm, text='bkn:', font=('Arial', 15)).grid(row=0)
+        self.cookie_lab = Label(input_frm, text='cookie:', font=('Arial', 15)).grid(row=1)
+        self.bkn_ipt = StringVar()
+        self.e_bkn = Entry(input_frm, textvariable = self.bkn_ipt)
+        self.cookie_ipt = StringVar()
+        self.e_cookie = Entry(input_frm, textvariable = self.cookie_ipt)
+        self.e_bkn.grid(row=0, column=1)
+        self.e_cookie.grid(row=1, column=1)
+        input_frm.pack()
+
+        self.save = Button(top, text='save', command=self.save)
+        self.save.pack()
+        if os.path.exists('biuflower.json'):
+            with open("biuflower.json","r") as cfg:
+                cfg_json = json.loads(cfg.read())
+                cookie_dic["cookie"] = cfg_json["cookie"]
+                cookie_dic['bkn'] = cfg_json['bkn']
+            self.e_bkn.insert(0,cookie_dic['bkn'])
+            self.e_cookie.insert(0,cookie_dic['cookie'])
+    def save(self):
+        cookie_dic["cookie"] = self.e_cookie.get()
+        cookie_dic["bkn"] = self.e_bkn.get()
+        with open("biuflower.json",'w') as cfg:
+            cfg_json = json.dumps(cookie_dic)
+            cfg.write(cfg_json)
+        tkMessageBox.showinfo("设置", "设置已保存")
+        self.top.destroy()
+def set_win():
+    inputDialog = Setting_window(root)
+    root.wait_window(inputDialog.top)
+
+
 def biu():
     '''start biu
     '''
-    gc = gc_ipt.get()
-    bkn = bkn_ipt.get()
-    # cookie = raw_input("cookie:")
-    poi = poi_ipt.get().encode("utf-8")
-    data = {}
-    # data['gc'] = gc
-    # data['bkn'] = bkn
-    # data["poi"] = poi
-    # data["is_sign"] = "1"
-    # data["from"] = "1"
-    data["poi"] = poi
+    if os.path.exists('biuflower.json'):
+        gc = gc_ipt.get()
+        bkn = cookie_dic["bkn"]
+        cookie = cookie_dic["cookie"]
+        poi = poi_ipt.get().encode("utf-8")
+        data = {}
 
-    #data encode as utf-8 urlencode.same as encode by dictionary
-    string = "gc="+gc+"&is_sign=0&from=1&bkn="+bkn+"&"+urllib.urlencode(data)
-    url = "http://qiandao.qun.qq.com/cgi-bin/sign"
-    req = urllib2.Request(url)
+        data["poi"] = poi
 
-    # post_data = urllib.urlencode(data)
-    # print post_data
-    print string
+        #data encode as utf-8 urlencode.same as encode by dictionary
+        string = "gc="+gc+"&is_sign=0&from=1&bkn="+bkn+"&"+urllib.urlencode(data)
+        url = "http://qiandao.qun.qq.com/cgi-bin/sign"
+        req = urllib2.Request(url)
 
+        print string
 
-    with open("header.txt",'r') as fp_head: #Load header file
-        head_dic = {}
-
-        for line in fp_head.readlines():
-            line_lst = line.replace('\n','').replace('\r','').split(": ")
-            if line_lst[0]:
-                head_dic[line_lst[0]] = line_lst[1]
-        for key in head_dic:
-            req.add_header(key,head_dic[key])
+        req_dic = {
+            'host': "qiandao.qun.qq.com",
+            'content-length': "37",
+            'accept': "*/*",
+            'content-type': "application/xml",
+            'origin': "http//qiandao.qun.qq.com",
+            'user-agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.59 QQ/7.4.15203.201 Safari/537.36",
+            'referer': "http//qiandao.qun.qq.com/index.html",
+            'accept-language': "en-us,en",
+            }
+        for key in req_dic:
+            req.add_header(key,req_dic[key])
         req.add_header("Content-Length",len(string)) #add COntent-Length
+        req.add_header("cookie",cookie) #add COntent-Length
 
+        # content = post(req,post_data)
+        content = post(req,string)
+        #decompress as gzip
+        if( ("Content-Encoding" in respInfo) and (respInfo['Content-Encoding'] == "gzip")) :
+            content = zlib.decompress(content, 16+zlib.MAX_WBITS)
+        print content.decode("utf-8").encode("gbk")
 
-    # content = post(req,post_data)
-    content = post(req,string)
-    #decompress as gzip
-    if( ("Content-Encoding" in respInfo) and (respInfo['Content-Encoding'] == "gzip")) :
-        content = zlib.decompress(content, 16+zlib.MAX_WBITS)
-    print content.decode("utf-8").encode("gbk")
-
-    content_dic = json.loads(content)
-    if content_dic["ec"] == 0:
-        tkMessageBox.showinfo("biu", "快去看看成功了没！")
-    elif content_dic["ec"] == 1:
-        tkMessageBox.showinfo("biu", "cookie过期了。重新抓包吧")
+        content_dic = json.loads(content)
+        if content_dic["ec"] == 0:
+            tkMessageBox.showinfo("biu", "快去看看成功了没！")
+        elif content_dic["ec"] == 1:
+            tkMessageBox.showinfo("biu", "cookie过期了。重新抓包吧")
+        else:
+            tkMessageBox.showinfo("biu", "出错啦。检查看看有没有什么地方填错了")
     else:
-        tkMessageBox.showinfo("biu", "出错啦。检查看看有没有什么地方填错了")
-
+        tkMessageBox.showinfo("biu", "先设置bkn和cookie再biu吧")
 # begin of GUI part
 root = Tk()
 root.title("签到练手GUI")
-root.geometry('400x120')                 #是x 不是*
-root.resizable(width=False, height=True) #宽不可变, 高可变,默认为True
+root.geometry('280x100')                 #是x 不是*
+root.resizable(width=False, height=False) #宽不可变, 高可变,默认为True
 frm = Frame(root)
-#left
-frm_L = Frame(frm)
-Label(frm_L, text='gc:'.decode('gbk').encode('utf8'), font=('Arial', 15)).pack(side=TOP)
-Label(frm_L, text='bkn:'.decode('gbk').encode('utf8'), font=('Arial', 15)).pack(side=TOP)
-Label(frm_L, text='poi:'.decode('gbk').encode('utf8'), font=('Arial', 15)).pack(side=TOP)
-frm_L.pack(side=LEFT)
 
-#right
-frm_R = Frame(frm)
+Label(frm, text='gc:', font=('Arial', 15)).grid(row=0)
+Label(frm, text='poi:', font=('Arial', 15)).grid(row=1)
+
 gc_ipt = StringVar()
-e1 = Entry(frm_R, textvariable = gc_ipt).pack(side=TOP)
-bkn_ipt = StringVar()
-e2 = Entry(frm_R, textvariable = bkn_ipt).pack(side=TOP)
+e_gc = Entry(frm, textvariable = gc_ipt)
 poi_ipt = StringVar()
-e2 = Entry(frm_R, textvariable = poi_ipt).pack(side=TOP)
-frm_R.pack(side=RIGHT)
-#Mid
-frm_B = Frame(root)
-Button(frm_B, text="biu", command=biu, width=10, height=1, font=('Arial', 10)).pack(side=TOP)
-frm_B.pack(side=TOP)
+e_poi = Entry(frm, textvariable = poi_ipt)
+e_gc.grid(row=0, column=1)
+e_poi.grid(row=1, column=1)
 
+#Button
+Button(frm, text="set", command=set_win, width=10, height=1, font=('Arial', 10)).grid(row=3, column=0)
+Button(frm, text="biu", command=biu, width=10, height=1, font=('Arial', 10)).grid(row=3, column=1)
 
 frm.pack()
-
+cookie_dic = {}
 root.mainloop()
